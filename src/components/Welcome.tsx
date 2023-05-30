ce1686f05d97920b650ce45eeae490242f8f3ccd
Allan Aguilar <alan-joc@hotmail.com>
2023-05-30 17:56:40 -0600

    Second Commit

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -20,7 +20,7 @@ export const Welcome: React.FC = () => {
                 <Box>
                     <Typography variant='body1' mt={1} sx={{ padding:'0 20px', fontSize:'18px', fontFamily:'Pangolin' }}><span style={{ fontSize:'24px', }}>Thank you</span> for visiting my Online Resume today.</Typography>
                     <Typography variant='body1' mt={1} sx={{ padding:'0 20px', fontSize:'18px', fontFamily:'Pangolin' }}><span style={{ fontSize:'24px', }}>I'm Allan</span>, I'm a Front End Developer, I have over 10 years of work experience, I'm currently working as a Front End Developer for Critical Mass, with previous experience working for prestigious Corporates like Intel Corporation, Western Union, Cisco Systems and Citi Bank.</Typography>
-                    <Typography variant='body1' mt={1} sx={{ padding:'0 20px', fontSize:'18px', fontFamily:'Pangolin' }}><span style={{ fontSize:'24px', }}>I've developed</span> this Online Resume with these technologies,</Typography>
+                    <Typography variant='body1' mt={1} sx={{ padding:'0 20px', fontSize:'18px', fontFamily:'Pangolin' }}><span style={{ fontSize:'24px', }}>I've Developed</span> this Online Resume with these technologies,</Typography>
                 </Box>
             </Container>
         </>
